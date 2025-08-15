2e5deb90285d250ef639836e60eecc555c61474a
Mujib ur Rahman <mujibkhan081@gmail.com>
2025-08-15 14:21:58 +0500

    Create 3-modules.js

---
+++ 3-modules.js
@@ -0,0 +1,12 @@
+//Modules -info about current module (file)
+
+const john = 'john'
+const peter = 'peter'
+const mujib = 'mujib'
+
+const sayHi = (name) => {
+    console.log(`Hello there ${name}`)
+}
+
+sayHi('susan')
+sayHi(mujib)

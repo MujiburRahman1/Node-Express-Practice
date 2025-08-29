393c802277e6481fb9e8a01cf45fe09d5fc782ae
Mujib ur Rahman <mujibkhan081@gmail.com>
2025-08-29 18:44:30 +0500

    Create 3-modules.js

---
+++ 01-node-js/3-modules.js
@@ -0,0 +1,14 @@
+//Modules -info about current module (file)
+//CommoneJS, every file is module (by default)
+
+const names = require('./4-names')
+const sayHi = require('./5-utlis')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+// console.log(data)
+// console.log(names)
+
+// sayHi('susan')
+// sayHi(names.mujib)
+// sayHi(names.john)
+// sayHi(names.peter)
